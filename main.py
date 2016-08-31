b4edda4f5fe7ef562433f790cb0cee34479b6d31
Aurelien Cibrario <aurelien.cibrario@gmail.com>
2016-08-31 20:24:28 +0200

    Ping rudimentaire en place

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ Simple Hello World
 """
 
 import tkinter as tk
+import os
+import subprocess
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -14,27 +16,38 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.button1 = tk.Button(self)
-        self.button1["text"] = "Click here to do action 1"
-        self.button1["command"] = self.action1
-        self.button1.pack(side="top")
+        self.ping_button = tk.Button(self)
+        self.ping_button["text"] = "Ping pour réveiller le tlhberry si la connection est compliquée"
+        self.ping_button["command"] = self.ping_action
+        self.ping_button.pack(side="top")
 
-        self.button2 = tk.Button(self)
-        self.button2["text"] = "Action 2 here"
-        self.button2["command"] = self.action2
-        self.button2.pack()
+        self.connect_button = tk.Button(self)
+        self.connect_button["text"] = "Action 2 here"
+        self.connect_button["command"] = self.connect_action
+        self.connect_button.pack()
 
-        self.output_label = tk.Label(self)
-        self.output_label["text"] = "Program will communicate here !"
-        self.output_label.pack(side="left")
+        self.output_var = tk.StringVar()
+        tk.Label(root, textvariable=self.output_var).pack(side="left")
+        self.output_var.set("Program will communicate here")
 
         self.quit = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
         self.quit.pack(side="bottom")
 
-    def action1(self):
-        print("action 1")
+    def ping_action(self):
+        # Ça marche pas le changement avant, je sais pas pourquoi
+        self.output_var.set("Dring dring ...")
+        # On pourrait boucler mais c'est trop long
+        #for ping in range(100,150):
+        address = "172.18.4.141"  # + str(ping)
+        res = subprocess.call(['ping', '-c', '3', address])
+        if res == 0:
+            self.output_var.set("ping to " + address + " OK")
+        elif res == 2:
+            self.output_var.set("pas de réponse de " + address)
+        else:
+            self.output_var.set("ÉCHEC du ping")
 
-    def action2(self):
+    def connect_action(self):
         self.output_label["text"] = "Ahah, t'a cliqué sur le bouton 2, je le sais"
 
 
